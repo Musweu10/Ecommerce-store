2699ccc8792a94417832fa63ee324f2224462efa
Musweu10 <53639461+Musweu10@users.noreply.github.com>
2020-12-08 16:47:23 -0800

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
+
+class ProductList extends Component {
+  // state = {
+  //   products: storeProducts,
+  // };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="Our" title="Products" />
+            {/* Product Row */}
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  // console.log(value);
+                  return value.products.map((product) => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+                {/* {(value) => {
+                  return <h1>{value}</h1>;
+                }} */}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+
+      //    <Product/>
+    );
+  }
+}
+
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { BrowserRouter, Switch as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ProductProvider } from './context';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ProductProvider>
+    <React.StrictMode>
+    <BrowserRouter>
+      <Router>
+        <App />
+      </Router>
+    </BrowserRouter>
+  </React.StrictMode>
+  </ProductProvider>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
