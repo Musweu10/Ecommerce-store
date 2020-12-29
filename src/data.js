df69b341f3f19b909f4634574e57eb7e8b0f944d
Musweu10 <53639461+Musweu10@users.noreply.github.com>
2020-12-28 20:17:01 -0800

    beats added

--- src/data.js
+++ src/data.js
@@ -301,10 +301,10 @@ export const storeProducts = [
   },
   {
     id: 26,
-    title: "HeadSets",
+    title: "Beats Solo",
     img: "img/headsets.png",
     price: 170,
-    company: "UCE",
+    company: "Beats",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
